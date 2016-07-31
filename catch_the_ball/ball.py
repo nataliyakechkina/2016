f99297460dee826a26773d4a718410939abdce84
Наталия <nataliyabaranova@yandex.ru>
2016-07-31 22:17:33 +0300

    Эксперименты с Tkinter дополнения

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -17,16 +17,21 @@ def print_hello(event):
     else:
         raise ValueError()
 
+def init_main_window():
+    global root, button1, button2, label, text, scale
+    root=tkinter.Tk()
+    button1=tkinter.Button(root, text="Button 1", command=button1_command)
+    button1.bind("<Button>",print_hello)
+    button2=tkinter.Button(root, text="Button 2")
+    button2.bind("<Button>",print_hello)
+    variable=tkinter.IntVar(0)
+    label=tkinter.Label(root, textvariable=variable)
+    scale=tkinter.Scale(root, orient=tkinter.HORIZONTAL, length=300, from_=0, to=100, tickinterval=10, resolution=5,variable=variable)
+    text=tkinter.Entry(root,  textvariable=variable)
 
+    for obj in  button1, button2, label,  scale, text:
+        obj.pack()
 
-
-root=tkinter.Tk()
-
-button1=tkinter.Button(root, text="Button 1", command=button1_command)
-button1.bind("<Button>",print_hello)
-button1.pack()
-button2=tkinter.Button(root, text="Button 2")
-button2.bind("<Button>",print_hello)
-button2.pack()
-
-root.mainloop()
+if __name__=="__main__":
+    init_main_window()
+    root.mainloop()
